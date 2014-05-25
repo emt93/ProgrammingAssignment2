081832484a25bb66efdb75dc5871b3789419ff69
Eric Tichbourne <etichbourne@gmail.com>
2014-05-25 16:14:07 -0400

    Solution to Programming Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below, I've created two functions which: 1. makes 2. stores 
+## 3. recalls a matrix and it's inverse. All of this is done using 
+## the cache.
 
-## Write a short comment describing this function
+## Just like the example shown in the layout of this problem, I created
+## the makeCacheMatrix function which builds a custom matrix that can run up to 
+## four function. Within the makeCacheMatrix, a function called "set" stores the 
+## matrix in the cache. And then the function called "get" retrieves it.
+## The functions "setInverse" and "getInverse" do the same thing but for
+## the inverse matrix of the original. 
+
+## The source of my logic amplied in this solution is the vector example
+## stated in the layout of this problem.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        n<-NULL
+        set<-function(y){
+                x<<-y
+                n<<-NULL 
+        }
+        get<-function()x
+        setInverse<-funtion(solve)n<<-solve
+        getInverse<-funtion()n
+        list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## The function cacheSolve takes the matrix built and stored by the makeCacheMatrix
+## function to find it's inverse. It's first task is to check the cache to make 
+## sure that the previous calculation has been done. If the result is that the
+## calculation wasn't done then this function calculates the inverse and stores
+## it in the cache. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        n<-x$getInverse()
+        if(!is.null(n)){
+                message("getting cached data")
+                return(n)
+        }
+        data<-x$get()
+        n<-solve(data, ...)
+        x$setInverse(n)
 }
